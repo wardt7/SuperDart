48cbce60efa23cc7ee87b03e47073f1c2f6408b5
Toby Ward <tobyfrederickward@gmail.com>
2020-08-27 17:22:10 +0100

    Updates

---
+++ precache-manifest.1eccfd212a2e8b9fcd5c2df32fbdf07c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb595d6e113cce9f8e6f",
+    "url": "/SuperDart/static/css/main.2ead1822.chunk.css"
+  },
+  {
+    "revision": "cb595d6e113cce9f8e6f",
+    "url": "/SuperDart/static/js/main.cb595d6e.chunk.js"
+  },
+  {
+    "revision": "aa1ac40b0b6f43ccac2a",
+    "url": "/SuperDart/static/css/1.7bd462dd.chunk.css"
+  },
+  {
+    "revision": "aa1ac40b0b6f43ccac2a",
+    "url": "/SuperDart/static/js/1.aa1ac40b.chunk.js"
+  },
+  {
+    "revision": "d2766889ef9ef14d8286",
+    "url": "/SuperDart/static/js/runtime~main.d2766889.js"
+  },
+  {
+    "revision": "b22c62a354e0aff325e091b1d234ecdf",
+    "url": "/SuperDart/index.html"
+  }
+];
